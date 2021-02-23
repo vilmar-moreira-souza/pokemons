4596c06406e106e47e4c67262eb767e97f02383c
vilmar <vilmarmoreira2010@gmail.com>
2021-02-23 03:42:41 -0300

    ad readme e paginação

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # pokemons
-mostra informaçoes de pokemons usando pokeApi
+mostra imagens,nome e id de pokemons usando pokeApi
+
+
+<a href="https://vilmar-moreira-souza.github.io/pokemons/" target="blank">visualizar</a>
+<h2>desktop</h2>
+<img src="./assets/Captura0.png" width="340" alt="print da tela">
+<h2>mobile</h2>
+<img src="./assets/Captura1.png" width="340"  alt="print da tela">
+<br>
+
+<h3>sobre a aplicação</h3>
+
+
+<p>essa aplicação busca dados de pokemons da api pokeApi e mostra na tela,é possivel obeter varia informaçoes. nessa implementaçõa foi utilizada imagens com o nome e id   </p>
+
+ <a href="https://vilmar-moreira-souza.github.io/pokemons/" target="blank">visualizar </a>
+ 
+
+<h3>ideias para implementar</h3>
+
+ <li>melhorar o design</li>
+<li> adicionar mais dados dos pokemons por exemplo habilidades</li>
+<li> adicionar mais dados de outraas fontes</li>
+<li> mais...</li>
+
+<h3>bugs e problemas</h3>
+
+<li>melhorar css- deixar codigo mais enxuto  </li>
+<li>refatorar codigo javascript implementando funçoes para um codigo mais limpo   </li>
+<li> mais...</li>
--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 const pokeDiv = document.getElementById("masterDiv")
-
+const proximo = document.getElementById("proximo")
+const anterior = document.getElementById("anterior")
+const botao1 = document.getElementById("1")
+const botao2 = document.getElementById("2")
+const botao3 = document.getElementById("3")
+const posicao = document.querySelector(".p1")
+posicao.value = ""
 
 function tela(i, nome, id) {
     let imagen = `<div class='bord'><img src='https://pokeres.bastionbot.org/images/pokemon/${i}.png'><span>nome : ${nome}</span><span>Id : ${id}</span></div>`
-    pokeDiv.innerHTML += imagen
+    onload(pokeDiv.innerHTML += imagen)
 }
+
 
 //posição 0 nao existe imagens- começar com 1 
 for (var i = 1; i <= 50; i++) {
@@ -15,14 +22,140 @@ for (var i = 1; i <= 50; i++) {
             .then(res => res.json())
             .then(data => {
                 (data.name),
-                tela(num, data.name, num)
+                    tela(num, data.name, num)
             }// num seria data.id
-                //console.log(data.sprites.other.dream_world.front_default)     imagens dos pokemons          
+                //console.log(data.sprites.other.dream_world.front_default)     imagens dos pokemons      
 
             )
             .catch(er => console.log(er))
     }
     pokeapi()
 
+
+}
+const md1 = () => {
+    if (posicao.value == "ola1") {
+
+    } else {
+        pokeDiv.innerHTML = ""
+
+        for (var i = 1; i <= 50; i++) {
+            let num = i
+            const pokeapi = () => {
+                const url = "https://pokeapi.co/api/v2/pokemon/" + i
+                fetch(url)
+                    .then(res => res.json())
+                    .then(data => {
+                        (data.name),
+                            tela(num, data.name, num)
+                    }// num seria data.id
+                        //console.log(data.sprites.other.dream_world.front_default)     imagens dos pokemons         
+
+                    )
+                    .catch(er => console.log(er))
+            }
+            pokeapi()
+        }
+        posicao.value = "ola1"
+        botao1.style.backgroundColor = "blue"
+        botao2.style.backgroundColor = "rgb(0, 255, 255)"
+        botao3.style.backgroundColor = "rgb(0, 255, 255)"
+    }
+
 }
 
+const md2 = () => {
+
+    if (posicao.value == "ola2") {
+
+    } else {
+        pokeDiv.innerHTML = ""
+        for (var i = 51; i <= 100; i++) {
+            let num = i
+            const pokeapi = () => {
+                const url = "https://pokeapi.co/api/v2/pokemon/" + i
+                fetch(url)
+                    .then(res => res.json())
+                    .then(data => {
+                        (data.name),
+                            tela(num, data.name, num)
+                    }// num seria data.id
+                        //console.log(data.sprites.other.dream_world.front_default)     imagens dos pokemons         
+
+                    )
+                    .catch(er => console.log(er))
+            }
+            pokeapi()
+        }
+        posicao.value = "ola2"
+        botao2.style.backgroundColor = "blue"
+        botao1.style.backgroundColor = "rgb(0, 255, 255)"
+        botao3.style.backgroundColor = "rgb(0, 255, 255)"
+    }
+
+}
+
+const md3 = () => {
+    if (posicao.value == "ola3") {
+
+    } else {
+
+        pokeDiv.innerHTML = ""
+        for (var i = 101; i <= 150; i++) {
+            let num = i
+            const pokeapi = () => {
+
+                const url = "https://pokeapi.co/api/v2/pokemon/" + i
+                fetch(url)
+                    .then(res => res.json())
+                    .then(data => {
+                        (data.name),
+                            tela(num, data.name, num)
+
+                    }// num seria data.id
+                        //console.log(data.sprites.other.dream_world.front_default)     imagens dos pokemons         
+
+                    )
+                    .catch(er => console.log(er))
+            }
+
+            pokeapi()
+        }
+        posicao.value = "ola3"
+        botao3.style.backgroundColor = "blue"
+        botao1.style.backgroundColor = "rgb(0, 255, 255)"
+        botao2.style.backgroundColor = "rgb(0, 255, 255)"
+    }
+}
+//botao1
+botao1.addEventListener('click', md1)
+//botao2
+botao2.addEventListener('click', md2)
+//botao3
+
+botao3.addEventListener('click', md3)
+
+//proximo
+proximo.addEventListener('click', () => {
+    if (posicao.value == "ola3") {
+    } else
+        if (posicao.value == "ola2") {
+            md3()
+        } else
+            if (posicao.value == "ola1") {
+                md2()
+            }
+})
+
+//anterior
+anterior.addEventListener('click', () => {
+    if (posicao.value == "ola3") {
+        md2()
+    } else
+        if (posicao.value == "ola2") {
+            md1()
+        } else
+            if (posicao.value == "ola1") {
+
+            }
+})
